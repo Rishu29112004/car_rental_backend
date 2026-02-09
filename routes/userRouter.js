468493107f9f5623a45fcee0b139dc1e827dfe42
rishu29112004 <kumarrishuritik1@gmail.com>
2026-02-10 01:44:10 +0530

    vercel deployment fix

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,19 +1,14 @@
 import express from "express";
 import authenticateUser from "../middleware/authMiddleware.js";
 import { updateMyProfile, getMyProfile } from "../controller/UserProfileController.js";
-import multer from "multer";
+import { multerMiddleware } from "../config/cloudanary.js";
 
 const profileRouter = express.Router();
 
-// Multer setup for file uploads
-const upload = multer({ dest: "uploads/" }); // temporary storage
-
 // Update profile with optional file
-profileRouter.put("/update/:id", authenticateUser, upload.single("imageUrl"), updateMyProfile);
+profileRouter.put("/update/:id", authenticateUser, multerMiddleware.single("imageUrl"), updateMyProfile);
 
 // Get my profile
 profileRouter.get("/:id", authenticateUser, getMyProfile);
 
 export default profileRouter;
-
-
